75ea5b551978a5a49816038e4cc014abf6facf5f
adityagupta4213 <daemonad@protonmail.com>
2021-02-11 11:50:30 +0530

    Fixed 'no user data' login issue

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  ActivityIndicator,
   Image,
   Text,
   TextInput,
@@ -9,29 +10,47 @@ import {
 } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {firebase} from '../firebase/config';
+import vars from '../shared/globalVars';
 
 export default function Login({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(() => {
+      if (firebase.auth().currentUser) {
+        navigation.navigate('App');
+      }
+    });
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
   const onFooterLinkPress = () => {
     navigation.navigate('Signup');
   };
 
   const onLoginPress = () => {
+    setIsLoading(true);
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then((user) => {
         navigation.navigate('App');
+        setIsLoading(false);
       })
       .catch((error) => {
         alert(error);
+        setIsLoading(false);
       });
   };
 
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../assets/logo-xpense.png')}
+        style={styles.logo}
+      />
       <KeyboardAwareScrollView
         style={{flex: 1, width: '100%'}}
         keyboardShouldPersistTaps="always">
@@ -55,7 +74,19 @@ export default function Login({navigation}) {
           autoCapitalize="none"
         />
         <TouchableOpacity style={styles.button} onPress={() => onLoginPress()}>
-          <Text style={styles.buttonTitle}>Log in</Text>
+          <Text
+            style={[
+              styles.buttonTitle,
+              isLoading ? {display: 'none'} : {display: 'flex'},
+            ]}>
+            Log in
+          </Text>
+          <ActivityIndicator
+            animating={isLoading}
+            hidesWhenStopped={true}
+            color="#ffffff"
+            style={{position: 'absolute'}}
+          />
         </TouchableOpacity>
         <View style={styles.footerView}>
           <Text style={styles.footerText}>
@@ -74,12 +105,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: '#ffffff',
   },
   title: {},
   logo: {
     flex: 1,
     height: 120,
-    width: 90,
+    resizeMode: 'contain',
     alignSelf: 'center',
     margin: 30,
   },
@@ -87,7 +119,7 @@ const styles = StyleSheet.create({
     height: 48,
     borderRadius: 5,
     overflow: 'hidden',
-    backgroundColor: 'white',
+    backgroundColor: '#efefef',
     marginTop: 10,
     marginBottom: 10,
     marginLeft: 30,
@@ -95,7 +127,7 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
   },
   button: {
-    backgroundColor: '#788eec',
+    backgroundColor: vars.colors.highlight,
     marginLeft: 30,
     marginRight: 30,
     marginTop: 20,
